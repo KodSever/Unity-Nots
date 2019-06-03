438cb562b66c30d2d8176dac87c4723af456708a
Yiğit KARAKOYUN <38832410+KodSever@users.noreply.github.com>
2019-06-03 14:51:19 +0300

    Update README.cs

--- README.cs
+++ README.cs
@@ -8,6 +8,7 @@ Return          Return key.
 Pause           Pause on PC machines.
 Escape          Escape key.
 Space           Space key.
+
 Keypad0         Numeric keypad 0.
 Keypad1         Numeric keypad 1.
 Keypad2         Numeric keypad 2.
@@ -25,6 +26,7 @@ KeypadMinus     Numeric keypad '-'.
 KeypadPlus      Numeric keypad '+'.
 KeypadEnter     Numeric keypad Enter.
 KeypadEquals    Numeric keypad '='.
+
 UpArrow         Up arrow key.
 DownArrow       Down arrow key.
 RightArrow      Right arrow key.
@@ -34,6 +36,7 @@ Home            Home key.
 End             End key.
 PageUp          Page up.
 PageDown        Page down.
+
 F1              F1 function key.
 F2              F2 function key.
 F3              F3 function key.
@@ -49,6 +52,7 @@ F12             F12 function key.
 F13             F13 function key.
 F14             F14 function key.
 F15             F15 function key.
+
 Alpha0          The '0' key on the top of the alphanumeric keyboard.
 Alpha1          The '1' key on the top of the alphanumeric keyboard.
 Alpha2          The '2' key on the top of the alphanumeric keyboard.
@@ -59,6 +63,7 @@ Alpha6          The '6' key on the top of the alphanumeric keyboard.
 Alpha7          The '7' key on the top of the alphanumeric keyboard.
 Alpha8          The '8' key on the top of the alphanumeric keyboard.
 Alpha9          The '9' key on the top of the alphanumeric keyboard.
+
 Exclaim         Exclamation mark key '!'.
 DoubleQuote     Double quote key '"'.
 Hash            Hash key '#'.
@@ -66,6 +71,7 @@ Dollar          Dollar sign key '$'.
 Percent         Percent '%' key.
 Ampersand       Ampersand key '&'.
 Quote           Quote key '.
+
 LeftParen       Left Parenthesis key '('.
 RightParen      Right Parenthesis key ')'.
 Asterisk        Asterisk key '*'.
@@ -87,6 +93,7 @@ RightBracket    Right square bracket key ']'.
 Caret           Caret key '^'.
 Underscore      Underscore '_' key.
 BackQuote       Back quote key '`'.
+
 A               'a' key.
 B               'b' key.
 C               'c' key.
@@ -101,314 +108,164 @@ K               'k' key.
 L               'l' key.
 M               'm' key.
 N               'n' key.
-O
-'o' key.
-P
-'p' key.
-Q
-'q' key.
-R
-'r' key.
-S
-'s' key.
-T
-'t' key.
-U
-'u' key.
-V
-'v' key.
-W
-'w' key.
-X
-'x' key.
-Y
-'y' key.
-Z
-'z' key.
-LeftCurlyBracket
-Left curly bracket key '{'.
-Pipe
-Pipe '|' key.
-RightCurlyBracket
-Right curly bracket key '}'.
-Tilde
-Tilde '~' key.
-Numlock
-Numlock key.
-CapsLock
-Capslock key.
-ScrollLock
-Scroll lock key.
-RightShift
-Right shift key.
-LeftShift
-Left shift key.
-RightControl
-Right Control key.
-LeftControl
-Left Control key.
-RightAlt
-Right Alt key.
-LeftAlt
-Left Alt key.
-LeftCommand
-Left Command key.
-LeftApple
-Left Command key.
-LeftWindows
-Left Windows key.
-RightCommand
-Right Command key.
-RightApple
-Right Command key.
-RightWindows
-Right Windows key.
-AltGr
-Alt Gr key.
-Help
-Help key.
-Print
-Print key.
-SysReq
-Sys Req key.
-Break
-Break key.
-Menu
-Menu key.
-Mouse0
-The Left (or primary) mouse button.
-Mouse1
-Right mouse button (or secondary mouse button).
-Mouse2
-Middle mouse button (or third button).
-Mouse3
-Additional (fourth) mouse button.
-Mouse4
-Additional (fifth) mouse button.
-Mouse5
-Additional (or sixth) mouse button.
-Mouse6
-Additional (or seventh) mouse button.
-JoystickButton0
-Button 0 on any joystick.
-JoystickButton1
-Button 1 on any joystick.
-JoystickButton2
-Button 2 on any joystick.
-JoystickButton3
-Button 3 on any joystick.
-JoystickButton4
-Button 4 on any joystick.
-JoystickButton5
-Button 5 on any joystick.
-JoystickButton6
-Button 6 on any joystick.
-JoystickButton7
-Button 7 on any joystick.
-JoystickButton8
-Button 8 on any joystick.
-JoystickButton9
-Button 9 on any joystick.
-JoystickButton10
-Button 10 on any joystick.
-JoystickButton11
-Button 11 on any joystick.
-JoystickButton12
-Button 12 on any joystick.
-JoystickButton13
-Button 13 on any joystick.
-JoystickButton14
-Button 14 on any joystick.
-JoystickButton15
-Button 15 on any joystick.
-JoystickButton16
-Button 16 on any joystick.
-JoystickButton17
-Button 17 on any joystick.
-JoystickButton18
-Button 18 on any joystick.
-JoystickButton19
-Button 19 on any joystick.
-Joystick1Button0
-Button 0 on first joystick.
-Joystick1Button1
-Button 1 on first joystick.
-Joystick1Button2
-Button 2 on first joystick.
-Joystick1Button3
-Button 3 on first joystick.
-Joystick1Button4
-Button 4 on first joystick.
-Joystick1Button5
-Button 5 on first joystick.
-Joystick1Button6
-Button 6 on first joystick.
-Joystick1Button7
-Button 7 on first joystick.
-Joystick1Button8
-Button 8 on first joystick.
-Joystick1Button9
-Button 9 on first joystick.
-Joystick1Button10
-Button 10 on first joystick.
-Joystick1Button11
-Button 11 on first joystick.
-Joystick1Button12
-Button 12 on first joystick.
-Joystick1Button13
-Button 13 on first joystick.
-Joystick1Button14
-Button 14 on first joystick.
-Joystick1Button15
-Button 15 on first joystick.
-Joystick1Button16
-Button 16 on first joystick.
-Joystick1Button17
-Button 17 on first joystick.
-Joystick1Button18
-Button 18 on first joystick.
-Joystick1Button19
-Button 19 on first joystick.
-Joystick2Button0
-Button 0 on second joystick.
-Joystick2Button1
-Button 1 on second joystick.
-Joystick2Button2
-Button 2 on second joystick.
-Joystick2Button3
-Button 3 on second joystick.
-Joystick2Button4
-Button 4 on second joystick.
-Joystick2Button5
-Button 5 on second joystick.
-Joystick2Button6
-Button 6 on second joystick.
-Joystick2Button7
-Button 7 on second joystick.
-Joystick2Button8
-Button 8 on second joystick.
-Joystick2Button9
-Button 9 on second joystick.
-Joystick2Button10
-Button 10 on second joystick.
-Joystick2Button11
-Button 11 on second joystick.
-Joystick2Button12
-Button 12 on second joystick.
-Joystick2Button13
-Button 13 on second joystick.
-Joystick2Button14
-Button 14 on second joystick.
-Joystick2Button15
-Button 15 on second joystick.
-Joystick2Button16
-Button 16 on second joystick.
-Joystick2Button17
-Button 17 on second joystick.
-Joystick2Button18
-Button 18 on second joystick.
-Joystick2Button19
-Button 19 on second joystick.
-Joystick3Button0
-Button 0 on third joystick.
-Joystick3Button1
-Button 1 on third joystick.
-Joystick3Button2
-Button 2 on third joystick.
-Joystick3Button3
-Button 3 on third joystick.
-Joystick3Button4
-Button 4 on third joystick.
-Joystick3Button5
-Button 5 on third joystick.
-Joystick3Button6
-Button 6 on third joystick.
-Joystick3Button7
-Button 7 on third joystick.
-Joystick3Button8
-Button 8 on third joystick.
-Joystick3Button9
-Button 9 on third joystick.
-Joystick3Button10
-Button 10 on third joystick.
-Joystick3Button11
-Button 11 on third joystick.
-Joystick3Button12
-Button 12 on third joystick.
-Joystick3Button13
-Button 13 on third joystick.
-Joystick3Button14
-Button 14 on third joystick.
-Joystick3Button15
-Button 15 on third joystick.
-Joystick3Button16
-Button 16 on third joystick.
-Joystick3Button17
-Button 17 on third joystick.
-Joystick3Button18
-Button 18 on third joystick.
-Joystick3Button19
-Button 19 on third joystick.
-Joystick4Button0
-Button 0 on forth joystick.
-Joystick4Button1
-Button 1 on forth joystick.
-Joystick4Button2
-Button 2 on forth joystick.
-Joystick4Button3
-Button 3 on forth joystick.
-Joystick4Button4
-Button 4 on forth joystick.
-Joystick4Button5
-Button 5 on forth joystick.
-Joystick4Button6
-Button 6 on forth joystick.
-Joystick4Button7
-Button 7 on forth joystick.
-Joystick4Button8
-Button 8 on forth joystick.
-Joystick4Button9
-Button 9 on forth joystick.
-Joystick4Button10
-Button 10 on forth joystick.
-Joystick4Button11
-Button 11 on forth joystick.
-Joystick4Button12
-Button 12 on forth joystick.
-Joystick4Button13
-Button 13 on forth joystick.
-Joystick4Button14
-Button 14 on forth joystick.
-Joystick4Button15
-Button 15 on forth joystick.
-Joystick4Button16
-Button 16 on forth joystick.
-Joystick4Button17
-Button 17 on forth joystick.
-Joystick4Button18
-Button 18 on forth joystick.
-Joystick4Button19
-Button 19 on forth joystick.
-Joystick5Button0
-Button 0 on fifth joystick.
-Joystick5Button1
-Button 1 on fifth joystick.
-Joystick5Button2
-Button 2 on fifth joystick.
-Joystick5Button3
-Button 3 on fifth joystick.
-Joystick5Button4
-Button 4 on fifth joystick.
-Joystick5Button5
-Button 5 on fifth joystick.
-Joystick5Button6
-Button 6 on fifth joystick.
-Joystick5Button7
-Button 7 on fifth joystick.
-Joystick5Button8
-Button 8 on fifth joystick.
-Joystick5Button9
-Button 9 on fifth joystick.
+O               'o' key.
+P               'p' key.
+Q               'q' key.
+R               'r' key.
+S               's' key.
+T               't' key.
+U               'u' key.
+V               'v' key.
+W               'w' key.
+X               'x' key.
+Y               'y' key.
+Z               'z' key.
+
+LeftCurlyBracket  Left curly bracket key '{'.
+Pipe              Pipe '|' key.
+RightCurlyBracket Right curly bracket key '}'.
+Tilde             Tilde '~' key.
+Numlock           Numlock key.
+CapsLock          Capslock key.
+ScrollLock        Scroll lock key.
+RightShift        Right shift key.
+LeftShift         Left shift key.
+RightControl      Right Control key.
+LeftControl       Left Control key.
+RightAlt          Right Alt key.
+LeftAlt           Left Alt key.
+LeftCommand       Left Command key.
+LeftApple         Left Command key.
+LeftWindows       Left Windows key.
+RightCommand      Right Command key.
+RightApple        Right Command key.
+RightWindows      Right Windows key.
+AltGr             Alt Gr key.
+Help              Help key.
+Print             Print key.
+SysReq            Sys Req key.
+Break             Break key.
+Menu              Menu key.
+
+Mouse0            The Left (or primary) mouse button.
+Mouse1            Right mouse button (or secondary mouse button).
+Mouse2            Middle mouse button (or third button).
+Mouse3            Additional (fourth) mouse button.
+Mouse4            Additional (fifth) mouse button.
+Mouse5            Additional (or sixth) mouse button.
+Mouse6            Additional (or seventh) mouse button.
+
+JoystickButton0   Button 0 on any joystick.
+JoystickButton1   Button 1 on any joystick.
+JoystickButton2   Button 2 on any joystick.
+JoystickButton3   Button 3 on any joystick.
+JoystickButton4   Button 4 on any joystick.
+JoystickButton5   Button 5 on any joystick.
+JoystickButton6   Button 6 on any joystick.
+JoystickButton7   Button 7 on any joystick.
+JoystickButton8   Button 8 on any joystick.
+JoystickButton9   Button 9 on any joystick.
+JoystickButton10  Button 10 on any joystick.
+JoystickButton11  Button 11 on any joystick.
+JoystickButton12  Button 12 on any joystick.
+JoystickButton13  Button 13 on any joystick.
+JoystickButton14  Button 14 on any joystick.
+JoystickButton15  Button 15 on any joystick.
+JoystickButton16  Button 16 on any joystick.
+JoystickButton17  Button 17 on any joystick.
+JoystickButton18  Button 18 on any joystick.
+JoystickButton19  Button 19 on any joystick.
+
+Joystick1Button0  Button 0 on first joystick.
+Joystick1Button1  Button 1 on first joystick.
+Joystick1Button2  Button 2 on first joystick.
+Joystick1Button3  Button 3 on first joystick.
+Joystick1Button4  Button 4 on first joystick.
+Joystick1Button5  Button 5 on first joystick.
+Joystick1Button6  Button 6 on first joystick.
+Joystick1Button7  Button 7 on first joystick.
+Joystick1Button8  Button 8 on first joystick.
+Joystick1Button9  Button 9 on first joystick.
+Joystick1Button10 Button 10 on first joystick.
+Joystick1Button11 Button 11 on first joystick.
+Joystick1Button12 Button 12 on first joystick.
+Joystick1Button13 Button 13 on first joystick.
+Joystick1Button14 Button 14 on first joystick.
+Joystick1Button15 Button 15 on first joystick.
+Joystick1Button16 Button 16 on first joystick.
+Joystick1Button17 Button 17 on first joystick.
+Joystick1Button18 Button 18 on first joystick.
+Joystick1Button19 Button 19 on first joystick.
+Joystick2Button0  Button 0 on second joystick.
+Joystick2Button1  Button 1 on second joystick.
+Joystick2Button2  Button 2 on second joystick.
+Joystick2Button3  Button 3 on second joystick.
+Joystick2Button4  Button 4 on second joystick.
+Joystick2Button5  Button 5 on second joystick.
+Joystick2Button6  Button 6 on second joystick.
+Joystick2Button7  Button 7 on second joystick.
+Joystick2Button8  Button 8 on second joystick.
+Joystick2Button9  Button 9 on second joystick.
+Joystick2Button10 Button 10 on second joystick.
+Joystick2Button11 Button 11 on second joystick.
+Joystick2Button12 Button 12 on second joystick.
+Joystick2Button13 Button 13 on second joystick.
+Joystick2Button14 Button 14 on second joystick.
+Joystick2Button15 Button 15 on second joystick.
+Joystick2Button16 Button 16 on second joystick.
+Joystick2Button17 Button 17 on second joystick.
+Joystick2Button18 Button 18 on second joystick.
+Joystick2Button19 Button 19 on second joystick.
+Joystick3Button0  Button 0 on third joystick.
+Joystick3Button1  Button 1 on third joystick.
+Joystick3Button2  Button 2 on third joystick.
+Joystick3Button3  Button 3 on third joystick.
+Joystick3Button4  Button 4 on third joystick.
+Joystick3Button5  Button 5 on third joystick.
+Joystick3Button6  Button 6 on third joystick.
+Joystick3Button7  Button 7 on third joystick.
+Joystick3Button8  Button 8 on third joystick.
+Joystick3Button9  Button 9 on third joystick.
+Joystick3Button10 Button 10 on third joystick.
+Joystick3Button11 Button 11 on third joystick.
+Joystick3Button12 Button 12 on third joystick.
+Joystick3Button13 Button 13 on third joystick.
+Joystick3Button14 Button 14 on third joystick.
+Joystick3Button15 Button 15 on third joystick.
+Joystick3Button16 Button 16 on third joystick.
+Joystick3Button17 Button 17 on third joystick.
+Joystick3Button18 Button 18 on third joystick.
+Joystick3Button19 Button 19 on third joystick.
+Joystick4Button0  Button 0 on forth joystick.
+Joystick4Button1  Button 1 on forth joystick.
+Joystick4Button2  Button 2 on forth joystick.
+Joystick4Button3  Button 3 on forth joystick.
+Joystick4Button4  Button 4 on forth joystick.
+Joystick4Button5  Button 5 on forth joystick.
+Joystick4Button6  Button 6 on forth joystick.
+Joystick4Button7  Button 7 on forth joystick.
+Joystick4Button8  Button 8 on forth joystick.
+Joystick4Button9  Button 9 on forth joystick.
+Joystick4Button10 Button 10 on forth joystick.
+Joystick4Button11 Button 11 on forth joystick.
+Joystick4Button12 Button 12 on forth joystick.
+Joystick4Button13 Button 13 on forth joystick.
+Joystick4Button14 Button 14 on forth joystick.
+Joystick4Button15 Button 15 on forth joystick.
+Joystick4Button16 Button 16 on forth joystick.
+Joystick4Button17 Button 17 on forth joystick.
+Joystick4Button18 Button 18 on forth joystick.
+Joystick4Button19 Button 19 on forth joystick.
+Joystick5Button0  Button 0 on fifth joystick.
+Joystick5Button1  Button 1 on fifth joystick.
+Joystick5Button2  Button 2 on fifth joystick.
+Joystick5Button3  Button 3 on fifth joystick.
+Joystick5Button4  Button 4 on fifth joystick.
+Joystick5Button5  Button 5 on fifth joystick.
+Joystick5Button6  Button 6 on fifth joystick.
+Joystick5Button7  Button 7 on fifth joystick.
+Joystick5Button8  Button 8 on fifth joystick.
+Joystick5Button9  Button 9 on fifth joystick.
 Joystick5Button10
 Button 10 on fifth joystick.
 Joystick5Button11
